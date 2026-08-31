a5f2c2ae158b6553a8ca5080ec8ce60d5a8f92dd
arnavsx3 <arnavsx3@gmail.com>
2026-08-31 13:38:22 +0530

    add job_repository.py to codebase

---
+++ src/cassian/services/job_repository.py
@@ -0,0 +1,21 @@
+from cassian.domain.models import JobView
+from cassian.infra.checkpoints import CheckpointStore
+
+
+class JobRepository:
+    def __init__(self, checkpoint_store: CheckpointStore) -> None:
+        self.checkpoint_store = checkpoint_store
+
+    def create(self, job: JobView) -> JobView:
+        self.checkpoint_store.save_job(job)
+        return job
+
+    def save(self, job: JobView) -> JobView:
+        self.checkpoint_store.save_job(job)
+        return job
+
+    def get(self, job_id: str) -> JobView | None:
+        return self.checkpoint_store.load_job(job_id)
+
+    def list(self) -> list[JobView]:
+        return self.checkpoint_store.load_all_jobs()
