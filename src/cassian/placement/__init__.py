66b97ebbe896043b7667fd6d1e247ec7ecc391da
arnavsx3 <arnavsx3@gmail.com>
2026-09-01 21:31:53 +0530

    add imports in placements/__init__.py

--- src/cassian/placement/__init__.py
+++ src/cassian/placement/__init__.py
@@ -8,22 +8,46 @@ from cassian.placement.cost_model import (
     estimate_spot_job_cost,
     interruption_probability_for_runtime,
 )
+from cassian.placement.eligibility import (
+    NoEligibleSpotPoolsError,
+    filter_eligible_profiles,
+    require_eligible_profiles,
+)
 from cassian.placement.models import (
     CostEstimate,
+    PlacementDecision,
+    PlacementStrategyName,
     RecoveryCostModel,
     SpotPoolProfile,
+    WorkerMarketType,
     WorkloadRequirements,
+)
+from cassian.placement.strategies import (
+    CheapestStrategy,
+    OnDemandStrategy,
+    RiskAwareStrategy,
+    get_strategy,
 )
 
 __all__ = [
     "SIMULATION_DATASET_VERSION",
     "SPOT_POOL_PROFILES",
+    "CheapestStrategy",
     "CostEstimate",
+    "NoEligibleSpotPoolsError",
+    "OnDemandStrategy",
+    "PlacementDecision",
+    "PlacementStrategyName",
     "RecoveryCostModel",
+    "RiskAwareStrategy",
     "SpotPoolProfile",
+    "WorkerMarketType",
     "WorkloadRequirements",
     "estimate_on_demand_job_cost",
     "estimate_spot_job_cost",
+    "filter_eligible_profiles",
     "get_spot_pool_profiles",
+    "get_strategy",
     "interruption_probability_for_runtime",
+    "require_eligible_profiles",
 ]
